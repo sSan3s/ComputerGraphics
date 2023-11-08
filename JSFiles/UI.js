89b0a0ec39203f3c33ab87509f20f37494593716
sSan3s <70534315+sSan3s@users.noreply.github.com>
2023-11-09 05:27:52 +0900

    Updating Skeletons
    Updating File List:
    
    JSFiles
    - Uploading JS Libraries
    - Uploading script.js
    - Uploading physics.js
    - Uploading orbitcontrols.js
    
    textures
    - New folder
    - Require textures of fruits (after)
    
    style.css
    - Fixing UI Style (after)
    
    app_config.json

---
+++ JSFiles/UI.js
@@ -0,0 +1,345 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { camera, renderer, 
+// createSph,
+createColorSph, guideLine, guideSphere, renewGuideSphere, fps, config, } from './script.js';
+import { sphs, side, height, setPhysicalParameters } from './physics.js';
+import { MathUtils, Vector3, Color } from 'three';
+let mouseX = 0, mouseY = 0, clickX = 0, clickY = 0; // Client mouse positions
+export let container = document.getElementById('container'); // container DOM
+export let w_width = container.clientWidth;
+export let w_height = container.clientHeight;
+export let w_ratio = w_height / w_width;
+let windowHalfX = container.clientWidth / 2 + container.offsetLeft;
+let windowHalfY = container.clientHeight / 2 + container.offsetTop;
+let containerWidth = container.clientWidth / 2;
+let containerHeight = container.clientHeight / 2;
+let upNextPanel = document.getElementById('upNext'); // Other UI panels DOM
+let scoreBoard = document.getElementById('scoreBoard');
+// Camera orientations and movement
+export let currentPhi = 0.25 * Math.PI + 0.001, targetPhi = 0.25 * Math.PI + 0.001;
+export let currentTheta = 0.25 * Math.PI, targetTheta = 0.25 * Math.PI;
+export let currentRadi = 900 * 1.5, targetRadi = 900 * 1.5;
+let transitionTime = 300;
+let noKeyInput = false;
+let isButtonDown = false;
+// Fruit rank (is it OK to be here...?) and game score.
+export let currentRank = MathUtils.randInt(0, 5);
+let nextRank = MathUtils.randInt(0, 5);
+let dropMargin = 48;
+let gameScore = 0;
+// Vectors used to map from client -> 3D world.
+let vec = new Vector3(); // recycle. normalized mouse position in camera view.
+let pos = new Vector3(); // recycle. world position under the mouse.
+// numbers for Mobile UIs
+let touchXstart = 0;
+let touchYstart = 0;
+export function onWindowResize() {
+    windowHalfX = container.clientWidth / 2 + container.offsetLeft;
+    windowHalfY = container.clientHeight / 2 + container.offsetTop;
+    containerWidth = container.clientWidth / 2;
+    containerHeight = container.clientHeight / 2;
+    // // Update cam
+    camera.aspect = containerWidth / containerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    dropMargin = config[4].radius + 1;
+}
+// PC mouse UIs
+let mouseMoveX;
+let mouseMoveY;
+export function onDocumentMouseMove(event) {
+    mouseMoveX = (event.clientX - windowHalfX + window.scrollX);
+    mouseMoveY = -(event.clientY - windowHalfY + window.scrollY);
+    vec.set(mouseMoveX / containerWidth, mouseMoveY / containerHeight, 1);
+    vec.unproject(camera);
+    vec.sub(camera.position).normalize();
+    pos.copy(camera.position).add(vec.multiplyScalar((0.5 * height + dropMargin - camera.position.z) / vec.z));
+    if (isInRange(pos, side + 50)) {
+        let margin = config[currentRank].radius * 0.75;
+        if (pos.x <= -side + margin)
+            pos.x = -side + margin;
+        else if (pos.x >= side - margin)
+            pos.x = side - margin;
+        if (pos.y <= -side + margin)
+            pos.y = -side + margin;
+        else if (pos.y >= side - margin)
+            pos.y = side - margin;
+        //@ts-ignore
+        guideLine.material.opacity = 0.5;
+        guideLine.position.set(pos.x, pos.y, 0);
+        guideSphere.position.set(pos.x, pos.y, 0.5 * height + dropMargin);
+        //@ts-ignore
+        guideSphere.material.opacity = 0.5;
+    }
+    else {
+        //@ts-ignore
+        guideLine.material.opacity = 0;
+        //@ts-ignore
+        guideSphere.material.opacity = 0;
+        // CameraSetting
+        if (isButtonDown) {
+            targetPhi = currentPhi + (mouseX - mouseMoveX) * 0.001 * Math.PI;
+            targetTheta = currentTheta + (mouseMoveY - mouseY) * 0.001 * Math.PI;
+            if (targetTheta > 0.5 * Math.PI)
+                targetTheta = 0.5 * Math.PI;
+            else if (targetTheta < 0)
+                targetTheta = 0;
+            setCameraStatus(targetPhi, targetTheta, targetRadi);
+            mouseX = mouseMoveX;
+            mouseY = mouseMoveY;
+            currentPhi = targetPhi;
+            currentTheta = targetTheta;
+        }
+    }
+}
+export function onDocumentClick(event) {
+    isButtonDown = true;
+    // mouseX = (event.clientX - windowHalfX + window.scrollX);
+    // mouseY = -(event.clientY - windowHalfY + window.scrollY);
+    // vec.set(mouseX / containerWidth, mouseY / containerHeight, 1);
+    // vec.unproject(camera);
+    // vec.sub(camera.position).normalize();
+    // pos.copy(camera.position).add(vec.multiplyScalar((0.5 * height + dropMargin - camera.position.z) / vec.z));
+    if (sphs.length > 0 &&
+        sphs[sphs.length - 1].mesh.position.z > (0.5 * height) - (sphs[sphs.length - 1].radius + config[currentRank].radius) &&
+        !sphs[sphs.length - 1].isEverCollide) {
+        return;
+    }
+    clickX = pos.x + (Math.random() - 0.5);
+    clickY = pos.y + (Math.random() - 0.5);
+    if (Math.abs(clickX) > (side + 50))
+        return;
+    if (Math.abs(clickY) > (side + 50))
+        return;
+    let margin = config[currentRank].radius * 0.75;
+    if (clickX <= -side + margin)
+        clickX = -side + margin;
+    else if (clickX >= side - margin)
+        clickX = side - margin;
+    if (clickY <= -side + margin)
+        clickY = -side + margin;
+    else if (clickY >= side - margin)
+        clickY = side - margin;
+    createColorSph(currentRank, [clickX, clickY, 0.5 * height + dropMargin], [Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, 0]); // This might not be random.
+    currentRank = nextRank;
+    nextRank = MathUtils.randInt(0, 5);
+    // // renew guide sphere.
+    renewGuideSphere();
+}
+export function onDocumentMouseUp(event) {
+    console.log("up");
+    isButtonDown = false;
+}
+export function onKeydown(event) {
+    let inputKey = event.key;
+    if (currentPhi >= 7)
+        currentPhi = 0;
+    if (noKeyInput)
+        return;
+    switch (inputKey.toUpperCase()) {
+        case 'W':
+            targetTheta = currentTheta - 0.1 * Math.PI;
+            break;
+        case 'A':
+            targetPhi = currentPhi - 0.25 * Math.PI;
+            break;
+        case 'S':
+            targetTheta = currentTheta + 0.1 * Math.PI;
+            break;    
+        case 'D':
+            targetPhi = currentPhi + 0.25 * Math.PI;
+            break;
+        case 'Q':
+            targetRadi = currentRadi - 80;
+            break;
+        case 'E':
+            targetRadi = currentRadi + 80;
+            break;
+        default:
+            break;
+    }
+    noKeyInput = true;
+    if (targetTheta < 0)
+        targetTheta = 0;
+    if (targetTheta > 0.5 * Math.PI)
+        targetTheta = 0.5 * Math.PI;
+    if (targetRadi < 100)
+        targetRadi = 100;
+    setTimeout(() => { noKeyInput = false; }, transitionTime);
+    smoothCameraSet(targetPhi, targetTheta, targetRadi);
+}
+
+// Mobile UIs
+export function onDocumentTouchStart(ev) {
+    touchXstart = (ev.touches[0].clientX - windowHalfX + window.scrollX);
+    touchYstart = -(ev.touches[0].clientY - windowHalfY + window.scrollY);
+}
+
+export function onDocumentTouched(ev) {
+    let clientX = (ev.changedTouches[0].clientX - windowHalfX + window.scrollX);
+    let clientY = -(ev.changedTouches[0].clientY - windowHalfY + window.scrollY);
+    vec.set(clientX / containerWidth, clientY / containerHeight, 1);
+    vec.unproject(camera);
+    vec.sub(camera.position).normalize();
+    pos.copy(camera.position).add(vec.multiplyScalar((0.5 * height + dropMargin - camera.position.z) / vec.z));
+    onDocumentClick(new MouseEvent("dummy")); // reuse PC version.
+    //@ts-ignore
+    guideLine.material.opacity = 0;
+    //@ts-ignore
+    guideSphere.material.opacity = 0;
+}
+
+export function onDocumentSwipe(ev) {
+    mouseX = (ev.changedTouches[0].clientX - windowHalfX + window.scrollX);
+    mouseY = -(ev.changedTouches[0].clientY - windowHalfY + window.scrollY);
+    vec.set(mouseX / containerWidth, mouseY / containerHeight, 1);
+    vec.unproject(camera);
+    vec.sub(camera.position).normalize();
+    pos.copy(camera.position).add(vec.multiplyScalar((0.5 * height + dropMargin - camera.position.z) / vec.z));
+    if (isInRange(pos, side + 50)) {
+        let margin = config[currentRank].radius * 0.75;
+        if (pos.x <= -side + margin)
+            pos.x = -side + +margin;
+        else if (pos.x >= side - margin)
+            pos.x = side - margin;
+        if (pos.y <= -side + margin)
+            pos.y = -side + margin;
+        else if (pos.y >= side - margin)
+            pos.y = side - margin;
+        //@ts-ignore
+        guideLine.material.opacity = 0.5;
+        guideLine.position.set(pos.x, pos.y, 0);
+        guideSphere.position.set(pos.x, pos.y, 0.5 * height + dropMargin);
+        //@ts-ignore
+        guideSphere.material.opacity = 0.5;
+    }
+    else {
+        targetPhi = currentPhi + (touchXstart - mouseX) * 0.001 * Math.PI;
+        targetTheta = currentTheta + (mouseY - touchYstart) * 0.001 * Math.PI;
+        if (targetTheta > 0.5 * Math.PI)
+            targetTheta = 0.5 * Math.PI;
+        else if (targetTheta < 0)
+            targetTheta = 0;
+        setCameraStatus(targetPhi, targetTheta, targetRadi);
+        touchXstart = mouseX;
+        touchYstart = mouseY;
+        currentPhi = targetPhi;
+        currentTheta = targetTheta;
+    }
+}
+function isInRange(pos, side) {
+    return (pos.x < side) && (pos.x > -side) && (pos.y < side) && (pos.y > -side);
+}
+// Set camera orientaion
+function setCameraStatus(Phi, Theta, Radi) {
+    let xyProjection = Radi * Math.sin(Theta);
+    let zAxis = new Vector3(0, 0, 1);
+    let yAxis = new Vector3(0, 1, 0);
+    camera.rotation.set(0, Theta, 0.5 * Math.PI); // 😢
+    camera.rotateOnWorldAxis(zAxis, Phi);
+    camera.position.set(xyProjection * Math.cos(Phi), xyProjection * Math.sin(Phi), Radi * Math.cos(Theta));
+}
+export function smoothCameraSet(Phi, Theta, Radi) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let initTime = Date.now();
+        let progressing = 0;
+        let transit = setInterval(() => {
+            progressing = (Date.now() - initTime) / transitionTime;
+            if (progressing >= 1) {
+                setCameraStatus(Phi, Theta, Radi);
+                clearInterval(transit);
+                currentPhi = targetPhi;
+                currentRadi = targetRadi;
+                currentTheta = targetTheta;
+            }
+            setCameraStatus(MathUtils.lerp(currentPhi, Phi, progressing), MathUtils.lerp(currentTheta, Theta, progressing), MathUtils.lerp(currentRadi, Radi, progressing));
+        }, 20);
+    });
+}
+// Scoring.
+export function addGameScore(num) {
+    gameScore += num;
+}
+// Display socre, next fruit.
+export function display() {
+    upNextPanel.innerText = config[nextRank].name; //nextRank.toString();
+    upNextPanel.style.color = "#" + new Color(config[nextRank].color).getHexString();
+    // upNextPanel.style.fontSize = config[nextRank].radius.toString()+"px";
+    scoreBoard.innerText = gameScore.toString();
+}
+/**
+ *
+ * Debugging, parameter adjust only
+ */
+export function debugging(debTab) {
+    var _a;
+    try {
+        debTab.item(0).innerHTML = "(" + pos.x.toFixed(2) + ", " + pos.y.toFixed(2) + ")";
+        debTab.item(1).innerHTML = "(" + windowHalfX + ", " + windowHalfY + ")";
+        debTab.item(2).innerHTML = "(" + camera.position.x.toFixed() + ", " + camera.position.y.toFixed() + ")";
+        // @ts-ignore
+        debTab.item(3).innerHTML = "(" + sphs.length + ")";
+        if (sphs.length > 0) {
+            let position = sphs[0].getPosFromMesh();
+            // @ts-ignore
+            debTab.item(4).innerHTML = "(" + Math.round(position[0]) + "," +
+                Math.round(position[1]) + "," +
+                Math.round(position[2]) + ")";
+            let vel = sphs[0].getVelFromMesh();
+            // @ts-ignore
+            debTab.item(5).innerHTML = "(" + Math.round(vel[0]) + "," +
+                Math.round(vel[1]) + "," +
+                Math.round(vel[2]) + ")";
+            debTab.item(6).innerHTML = "(" + fps.toPrecision(3) + ")";
+        }
+        let sphereDisplay = "";
+        for (let i = 0; i < sphs.length; i++) {
+            sphereDisplay += "<tr><td>" +
+                i + "</td><td>" +
+                sphs[i].rank.toFixed() + "</td><td>" +
+                sphs[i].radius.toFixed() + "</td><td>" +
+                sphs[i].mass.toFixed() +
+                "</td><tr>";
+        }
+        // @ts-ignore
+        (_a = debTab.namedItem("spheres")) === null || _a === void 0 ? void 0 : _a.innerHTML = sphereDisplay;
+        // @ts-ignore
+        setPhysicalParameters(
+        // @ts-ignore
+        Number(debTab.namedItem("floorE").value), 
+        // @ts-ignore
+        Number(debTab.namedItem("wallE").value), 
+        // @ts-ignore
+        Number(debTab.namedItem("spheE").value), 
+        // @ts-ignore
+        Number(debTab.namedItem("spheF").value), 
+        // @ts-ignore
+        Number(debTab.namedItem("still").value), 
+        // @ts-ignore
+        Number(debTab.namedItem("wallRep").value), 
+        // @ts-ignore
+        Number(debTab.namedItem("spheRep").value)); // 옘병~
+        console.log();
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+export let getAngle = 0;
+export function onCamDebugChanged(event) {
+    // @ts-ignore
+    let camDistance = event.currentTarget.camR.value;
+    // @ts-ignore
+    getAngle = Number(event.currentTarget.camTh.value);
+    // setCameraStatus((getAngle) * Math.PI, 0.25* Math.PI, 900 * 1.4);
+    setCameraStatus((getAngle) * Math.PI, 0.25 * Math.PI, camDistance);
+}
+//# sourceMappingURL=UI.js.map
---
+++ JSFiles/physics.js
@@ -0,0 +1,275 @@
+import * as THREE from "three";
+import { gameOver, rankUpSph, killSph } from "./script.js";
+import { addGameScore } from "./UI.js";
+let gravity = 0.098; // At what framerate? 120?
+const zAxis = new THREE.Vector3(0, 0, 1);
+const yAxis = new THREE.Vector3(0, 1, 0);
+const xAxis = new THREE.Vector3(1, 0, 0);
+export let G = new THREE.Vector3(0, 0, -gravity);
+export let sphs = []; // managing all fruits
+export let side = 100;
+export let height = 300;
+let halfHeight = height * 0.5;
+// Physical properties...
+let floorElasticity = 0.5;
+let sideWallElasticity = 0.7;
+let interSphereElasticity = 0.5;
+let sphereFriction = 0.6;
+let stillness = 4 * gravity;
+let wallOverwrapCoeff = 0.1;
+let overwrapRepulsion = 0.6; // = sphere repulsion
+let wallSpinFriction = 1.5;
+let tmp = new THREE.Vector3();
+export class Physical {
+    constructor(mesh, vel, rank, radius) {
+        this.mesh = mesh;
+        this.vel = new THREE.Vector3(vel[0], vel[1], vel[2]);
+        this.spin = new THREE.Vector3(0.0, 0.0, 0.0);
+        this.rank = rank;
+        this.radius = radius;
+        this.isCollide = false;
+        this.mass = 1;
+        this.isReservedToDestroyed = false;
+        this.isEverCollide = false;
+    }
+    getPosFromMesh() {
+        return [this.mesh.position.x, this.mesh.position.y, this.mesh.position.z];
+    }
+    getVelFromMesh() {
+        return [this.vel.x, this.vel.y, this.vel.z];
+    }
+    nextPosition() {
+        if (this.isCollide) {
+            // precalculated movement. skip this frame.
+            this.isCollide = false;
+        }
+        else {
+            this.mesh.position.add(this.vel);
+            this.accelerate(G);
+            tmp = this.spin.clone();
+            this.mesh.rotateOnWorldAxis(tmp.normalize(), this.spin.length());
+        }
+    }
+    accelerate(acc) {
+        this.vel.add(acc);
+    }
+    checkWallCollision() {
+        // floor (set this -height )
+        let overwrap = this.radius - (this.mesh.position.z + this.vel.z) - halfHeight;
+        if (overwrap > 0) {
+            // simple solution, no fast moving friends
+            this.isCollide = true;
+            this.isEverCollide = true;
+            this.mesh.position.x += this.vel.x;
+            this.mesh.position.y += this.vel.y;
+            this.vel.x *= 0.99;
+            this.vel.y *= 0.99; //friction
+            this.spin.z *= 0.99; // spin loss from friction
+            this.spin.x = -this.vel.y / this.radius;
+            this.spin.y = this.vel.x / this.radius; // spin affected by vel
+            this.vel.x = this.spin.y * this.radius;
+            this.vel.y = -this.spin.x * this.radius; // vel affected by spin
+            if (this.vel.length() < stillness) {
+                this.vel.z = 0;
+                this.mesh.position.z = this.radius - halfHeight;
+            }
+            else {
+                this.mesh.position.z = this.radius - halfHeight;
+                this.vel.z = Math.floor(Math.abs(this.vel.z) * 2) * 0.5 * floorElasticity;
+                this.mesh.position.z += this.vel.z;
+            }
+        }
+        // side wall edge (at entry)
+        let edgeDist = new THREE.Vector3();
+        let edgeCollisionVector = new THREE.Vector3();
+        if (this.mesh.position.z > halfHeight) {
+            edgeDist.set(this.mesh.position.x + this.vel.x - side, 0, this.mesh.position.z + this.vel.z - halfHeight);
+            if (edgeDist.length() < this.radius) {
+                this.isCollide = true;
+                edgeCollisionVector = this.vel.projectOnVector(edgeDist.normalize());
+                this.vel.add(edgeCollisionVector.negate());
+                this.spin.add(yAxis).multiplyScalar(-1.0 / this.radius); // spin
+            }
+            edgeDist.set(this.mesh.position.x + this.vel.x + side, 0, this.mesh.position.z + this.vel.z - halfHeight);
+            if (edgeDist.length() < this.radius) {
+                this.isCollide = true;
+                edgeCollisionVector = this.vel.projectOnVector(edgeDist.normalize());
+                this.vel.add(edgeCollisionVector.negate());
+                this.spin.add(yAxis).multiplyScalar(1.0 / this.radius); // spin
+            }
+            edgeDist.set(0, this.mesh.position.y + this.vel.y - side, this.mesh.position.z + this.vel.z - halfHeight);
+            if (edgeDist.length() < this.radius) {
+                this.isCollide = true;
+                edgeCollisionVector = this.vel.projectOnVector(edgeDist.normalize());
+                this.vel.add(edgeCollisionVector.negate());
+                this.spin.add(xAxis).multiplyScalar(1.0 / this.radius); // spin
+            }
+            edgeDist.set(0, this.mesh.position.y + this.vel.y + side, this.mesh.position.z + this.vel.z - halfHeight);
+            if (edgeDist.length() < this.radius) {
+                this.isCollide = true;
+                edgeCollisionVector = this.vel.projectOnVector(edgeDist.normalize());
+                this.vel.add(edgeCollisionVector.negate());
+                this.spin.add(xAxis).multiplyScalar(-1.0 / this.radius); // spin
+            }
+        }
+        else {
+            // side walls (inside the box)
+            let nextXpos = this.mesh.position.x + this.vel.x;
+            overwrap = nextXpos + this.radius - side;
+            if (overwrap > 0) {
+                this.isCollide = true;
+                this.vel.x = -Math.abs(this.vel.x) * sideWallElasticity;
+                this.mesh.position.x += this.vel.x;
+                // spin
+                this.vel.z += this.spin.y / this.radius * wallSpinFriction;
+                overwrap = this.mesh.position.x + this.radius - side;
+                while (overwrap > 0) {
+                    this.vel.x += -overwrap * wallOverwrapCoeff;
+                    this.mesh.position.x += this.vel.x;
+                    overwrap = this.mesh.position.x + this.radius - side;
+                }
+            }
+            overwrap = this.radius - nextXpos - side;
+            if (overwrap > 0) {
+                this.isCollide = true;
+                this.vel.x = Math.abs(this.vel.x) * sideWallElasticity;
+                this.mesh.position.x += this.vel.x;
+                // spin
+                this.vel.z -= this.spin.y / this.radius * wallSpinFriction;
+                overwrap = -this.mesh.position.x + this.radius - side;
+                while (overwrap > 0) {
+                    this.vel.x += overwrap * wallOverwrapCoeff;
+                    this.mesh.position.x += this.vel.x;
+                    overwrap = -this.mesh.position.x + this.radius - side;
+                }
+            }
+            let nextYpos = this.mesh.position.y + this.vel.y;
+            overwrap = nextYpos + this.radius - side;
+            if (overwrap > 0) {
+                this.isCollide = true;
+                this.vel.y = -Math.abs(this.vel.y) * sideWallElasticity;
+                this.mesh.position.y += this.vel.y;
+                // spin
+                this.vel.z += this.spin.x / this.radius * wallSpinFriction;
+                overwrap = this.mesh.position.y + this.radius - side;
+                while (overwrap > 0) {
+                    this.vel.y += -overwrap * wallOverwrapCoeff;
+                    this.mesh.position.y += this.vel.y;
+                    overwrap = this.mesh.position.y + this.radius - side;
+                }
+            }
+            overwrap = this.radius - nextYpos - side;
+            if (overwrap > 0) {
+                this.isCollide = true;
+                this.vel.y = Math.abs(this.vel.y) * sideWallElasticity;
+                this.mesh.position.y += this.vel.y;
+                // spin
+                this.vel.z -= this.spin.x / this.radius * wallSpinFriction;
+                overwrap = -this.mesh.position.y + this.radius - side;
+                while (overwrap > 0) {
+                    this.vel.y += overwrap * wallOverwrapCoeff;
+                    this.mesh.position.y += this.vel.y;
+                    overwrap = -this.mesh.position.y + this.radius - side;
+                }
+            }
+        }
+    }
+    // Collision between fruits
+    checkCollisionWith(x) {
+        let objA = this.mesh.position.clone();
+        let objB = x.mesh.position.clone();
+        if (this.radius + x.radius > (objA.add(this.vel)).distanceTo(objB.add(x.vel)) &&
+            !this.isReservedToDestroyed && !x.isReservedToDestroyed) {
+            if (this.radius === x.radius) {
+                this.sphereFusion(x);
+                return;
+            }
+            this.isCollide = true;
+            this.isEverCollide = true;
+            // MATH...!!! 😢
+            // Use below variables... It seems complicated.
+            let lineV = objA.sub(objB).normalize();
+            let normalVelA = this.vel.clone().projectOnVector(lineV);
+            let normalVelB = x.vel.clone().projectOnVector(lineV);
+            this.vel.sub(normalVelA).multiplyScalar(sphereFriction);
+            x.vel.sub(normalVelB).multiplyScalar(sphereFriction);
+            // spin part
+            this.spin.add(tmp.crossVectors(lineV, this.vel).multiplyScalar(1 / this.radius));
+            x.spin.add(tmp.crossVectors(x.vel, lineV).multiplyScalar(1 / x.radius));
+            this.spin.multiplyScalar(0.9);
+            x.spin.multiplyScalar(0.9);
+            // back to linear part
+            let amplA = normalVelA.dot(lineV); // this don't use sqrt!
+            let amplB = normalVelB.dot(lineV);
+            let massSum = this.mass + x.mass;
+            normalVelA = lineV.clone().multiplyScalar((amplA * (this.mass - x.mass) + amplB * (2 * x.mass)) / massSum);
+            normalVelB = lineV.clone().multiplyScalar((amplA * (2 * this.mass) + amplB * (x.mass - this.mass)) / massSum);
+            this.vel.add(normalVelA.multiplyScalar(interSphereElasticity));
+            x.vel.add(normalVelB.multiplyScalar(interSphereElasticity));
+            this.mesh.position.add(this.vel);
+            x.mesh.position.add(x.vel);
+            let overwrap = (this.radius + x.radius) - tmp.subVectors(this.mesh.position, x.mesh.position).length();
+            if (overwrap > 0) {
+                lineV.multiplyScalar(overwrap * 0.5); // reposition to just-contact.
+                this.mesh.position.add(lineV);
+                x.mesh.position.sub(lineV);
+                lineV.multiplyScalar(overwrapRepulsion);
+                x.vel.add(lineV);
+                this.vel.add(lineV);
+            }
+        }
+    }
+    sphereFusion(sph) {
+        this.mesh.position.add(sph.mesh.position).multiplyScalar(0.5);
+        this.vel.multiplyScalar(0);
+        this.spin.addVectors(this.spin, sph.spin).multiplyScalar(0.5);
+        this.isCollide = true;
+        rankUpSph(this);
+        this.isEverCollide = true;
+        addGameScore(this.rank ** 2);
+        killSph(sph);
+    }
+    checkGameOver() {
+        if (this.isEverCollide && this.mesh.position.z > 0.5 * height) {
+            gameOver();
+        }
+    }
+}
+;
+export function setPhysicalParameters(floorE, wallE, spheE, spheF, still, wallRepulse, spheRepulse) {
+    floorElasticity = floorE;
+    sideWallElasticity = wallE;
+    interSphereElasticity = spheE;
+    sphereFriction = spheF;
+    stillness = still * gravity;
+    wallOverwrapCoeff = wallRepulse;
+    overwrapRepulsion = spheRepulse;
+}
+// loop thru physcial objects.
+export function physics(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        // Collision with side wall and floor might be treated as special case?? it only cost O(N).
+        elements[i].checkWallCollision();
+        // spheres 사라지는 경우 주의...
+        for (let j = 0; j < elements.length; j++) {
+            if (i === j)
+                continue;
+            elements[i].checkCollisionWith(elements[j]);
+        }
+        // After all are done move to next position.
+        elements[i].nextPosition(); // and also accelerate.
+    }
+    // Remove sphes & game over check
+    for (let i = 0; i < elements.length; i++) {
+        try {
+            elements[i].checkGameOver();
+            if (elements[i].isReservedToDestroyed) {
+                elements = elements.splice(i, 1);
+            }
+        }
+        catch (e) {
+            break;
+        }
+    }
+}
+//# sourceMappingURL=physics.js.map
